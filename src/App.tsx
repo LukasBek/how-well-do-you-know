16ec01296bb37d155768dbf43143d9899477d73f
Lukas <lukas@starsite.dk>
2022-04-27 21:21:07 +0200

    changed a bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import Button from 'react-bootstrap/Button';
 
 // Components
 import QuestionCard from './components/QuestionCard'
+import { Container, Row, Col } from 'react-bootstrap';
 
 let userAnswers: string[] = [];
 
 const App = () => {
+
+  document.body.style.background = 'blanchedalmond';
 
   type Question = {
     question: string;
@@ -63,43 +66,47 @@ const App = () => {
 
 
   return (
-    <div className="App">
-      <h1>Konfirmations Quiz</h1>
-      <p></p>
-      {!quizStarted ? (
-        <Button variant="primary" onClick={startQuiz}>
-        Tryk for at starte
-        </Button>
-      ) : null}
-
-
-      {quizStarted && questionNumber < questionList.length ? 
-      <QuestionCard 
-        questionNumber={questionNumber+1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questionList[questionNumber].question}
-        answers={questionList[questionNumber].answers}
-        userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
-        callback={onAnswerClick}
-      />
-      : null}
-
-      {quizStarted && questionNumber+1 < questionList.length && currentlySelectedAnswer !== "" ? 
-            <Button className="next" onClick={nextQuestion}>
-              Næste spørgsmål
+    <Container fluid>
+      <div className="App">
+        <Row className="justify-content-md-center">
+          <h1>Konfirmations Quiz</h1>
+        </Row>
+        <Row className="justify-content-md-center">
+          {!quizStarted ? (
+            <Button variant="primary" className="col-2" onClick={startQuiz}>
+            Tryk for at starte
             </Button>
-      : quizStarted && questionNumber+1 == questionList.length && currentlySelectedAnswer !== "" ? 
-            <Button className="next" onClick={finishQuiz}>
-            Afslut quiz
-            </Button>
-      : null}
+          ) : null}
 
-      {quizStarted && questionNumber == questionList.length ? 
-      <p>quiz slut</p>
-      : null}
-      
-    </div>
-    
+
+          {quizStarted && questionNumber < questionList.length ? 
+          <QuestionCard 
+            questionNumber={questionNumber+1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questionList[questionNumber].question}
+            answers={questionList[questionNumber].answers}
+            userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
+            callback={onAnswerClick}
+          />
+          : null}
+        </Row>
+        <Row className="justify-content-md-center">
+        {quizStarted && questionNumber+1 < questionList.length && currentlySelectedAnswer !== "" ? 
+              <Button className="col-2" onClick={nextQuestion}>
+                Næste spørgsmål
+              </Button>
+        : quizStarted && questionNumber+1 === questionList.length && currentlySelectedAnswer !== "" ? 
+              <Button className="col-2" onClick={finishQuiz}>
+              Afslut quiz
+              </Button>
+        : null}
+        </Row>
+        {quizStarted && questionNumber === questionList.length ? 
+        <p>quiz slut</p>
+        : null}
+        
+      </div>
+    </Container>
   );
 }
 
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Container, Button, Col, Row } from 'react-bootstrap';
 
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
     userAnswer: any;
     questionNumber: number;
     totalQuestions: number;
 }
+
+
 
 const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNumber, totalQuestions}) => (
     <div>
@@ -16,18 +18,58 @@ const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer,
             Spørgsmål: {questionNumber} / {totalQuestions}
         </p>
         <p>{question}</p>
-        <div>
-            {answers.map(answer => (
-                <div key={answer}>
-                    <Button variant="secondary" disabled={userAnswer} value={answer} onClick={callback}>
-                        {answer}
+
+        <Row className="justify-content-md-center">
+            <Col xs={2}>
+                <div key={answers[0]}>
+                    <Button variant="secondary" size="lg" className="col-10" disabled={userAnswer} value={answers[0]} onClick={callback}>
+                        {answers[0]}
                     </Button>
                     <p></p>
                 </div>
-            ))}
-        </div>
+            </Col>
+            <Col xs={2}>
+                <div key={answers[1]}>
+                    <Button variant="secondary" size="lg" className="col-10" disabled={userAnswer} value={answers[1]} onClick={callback}>
+                        {answers[1]}
+                    </Button>
+                    <p></p>
+                </div>
+            </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+            <Col xs={2}>
+                <div key={answers[2]}>
+                    <Button variant="secondary" size="lg" className="col-10" disabled={userAnswer} value={answers[2]} onClick={callback}>
+                        {answers[2]}
+                    </Button>
+                    <p></p>
+                </div>
+            </Col>
+            <Col xs={2}>
+                <div key={answers[3]}>
+                    <Button variant="secondary" size="lg" className="col-10" disabled={userAnswer} value={answers[3]} onClick={callback}>
+                        {answers[3]}
+                    </Button>
+                    <p></p>
+                </div>
+            </Col>
+        </Row>
+            
+
+
     </div>
 );
 
+/**
+{answers.map(answer => (
+    <div key={answer}>
+        <Button variant="secondary" disabled={userAnswer} value={answer} onClick={callback}>
+            {answer}
+        </Button>
+        <p></p>
+    </div>
+))}
+ */
 
 export default QuestionCard;
