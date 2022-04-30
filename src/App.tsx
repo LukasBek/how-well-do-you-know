ae316284901bcb33ca2521a9cd1766eb988f536b
Lukas <lukas@starsite.dk>
2022-04-30 20:41:50 +0200

    fixed start button and adding padding from header

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,67 @@
-import React, { useState } from 'react';
-import './App.css';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
 
 // Components
-import QuestionCard from './components/QuestionCard'
-import { Container, Row, Col } from 'react-bootstrap';
+import QuestionCard from "./components/QuestionCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 let userAnswers: string[] = [];
 
 const App = () => {
-
-  document.body.style.background = 'blanchedalmond';
+  document.body.style.background = "blanchedalmond";
 
   type Question = {
     question: string;
     answers: string[];
-  }
-  
+  };
+
   const questionList: Question[] = [
     {
       question: "Hvad er Julies efternavn?",
-      answers: ["Hansen", "Petersen", "Lynge", "Andersen"]
+      answers: ["Hansen", "Petersen", "Lynge", "Andersen"],
     },
     {
       question: "Hvad vil Julie helst have til aftensmad?",
-      answers: ["Sushi", "Pizza", "Bøf", "Suppe"]
+      answers: ["Sushi", "Pizza", "Bøf", "Suppe"],
     },
     {
       question: "Hvilket land vil Julie helst til?",
-      answers: ["Frankrig", "Grønland", "Australien", "USA"]
-    }];
+      answers: ["Frankrig", "Grønland", "Australien", "USA"],
+    },
+  ];
 
-    const TOTAL_QUESTIONS = questionList.length;
+  const TOTAL_QUESTIONS = questionList.length;
 
   const [quizStarted, setQuizStarted] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(0);
   const [currentlySelectedAnswer, setCurrentlySelectedAnswer] = useState("");
 
-
   const startQuiz = () => {
-      setQuizStarted(true);
-      console.log("Start pressed");
-      console.log("Questions are: " + questionList);
-  }
+    setQuizStarted(true);
+    console.log("Start pressed");
+    console.log("Questions are: " + questionList);
+  };
 
   const onAnswerClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // user answer  
+    // user answer
     const answer = e.currentTarget.value;
-    
+
     setCurrentlySelectedAnswer(answer);
 
     console.log("answer is " + answer);
-  }
+  };
 
   const nextQuestion = () => {
     userAnswers.push(currentlySelectedAnswer);
     setCurrentlySelectedAnswer("");
-    setQuestionNumber(prev => prev + 1);
-  }
+    setQuestionNumber((prev) => prev + 1);
+  };
 
   const finishQuiz = () => {
     nextQuestion();
     console.log(userAnswers);
-  }
-
+  };
 
   return (
     <Container fluid="sm">
@@ -73,45 +71,58 @@ const App = () => {
             <h1>Konfirmations Quiz</h1>
           </Row>
           <Row className="justify-content-md-center">
+            <div className="content">
+              <Col md={4}>
+                {!quizStarted ? (
+                  <Button
+                    variant="primary"
+                    className="col-8"
+                    size="lg"
+                    onClick={startQuiz}
+                  >
+                    Tryk for at starte
+                  </Button>
+                ) : null}
+              </Col>
 
-            {!quizStarted ? (
-              <Button variant="primary" className="col-2" size="sm" onClick={startQuiz}>
-              Tryk for at starte
-              </Button>
-            ) : null}
-
-
-            {quizStarted && questionNumber < questionList.length ? 
-            <QuestionCard 
-              questionNumber={questionNumber+1}
-              totalQuestions={TOTAL_QUESTIONS}
-              question={questionList[questionNumber].question}
-              answers={questionList[questionNumber].answers}
-              userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
-              callback={onAnswerClick}
-            />
-            : null}
+              {quizStarted && questionNumber < questionList.length ? (
+                <QuestionCard
+                  questionNumber={questionNumber + 1}
+                  totalQuestions={TOTAL_QUESTIONS}
+                  question={questionList[questionNumber].question}
+                  answers={questionList[questionNumber].answers}
+                  userAnswer={
+                    userAnswers ? userAnswers[questionNumber] : undefined
+                  }
+                  callback={onAnswerClick}
+                />
+              ) : null}
+            </div>
           </Row>
           <Row className="justify-content-md-center">
-            <Col md={4}>  
-              {quizStarted && questionNumber+1 < questionList.length && currentlySelectedAnswer !== "" ? 
-                    <Button className="col-6" size="sm" onClick={nextQuestion}>
-                      Næste spørgsmål
-                    </Button>
-              : quizStarted && questionNumber+1 === questionList.length && currentlySelectedAnswer !== "" ? 
-                    <Button className="col-6" size="sm" onClick={finishQuiz}>
-                    Afslut quiz
-                    </Button>
-              : null}
+            <Col md={4}>
+              {quizStarted &&
+              questionNumber + 1 < questionList.length &&
+              currentlySelectedAnswer !== "" ? (
+                <Button className="col-6" size="sm" onClick={nextQuestion}>
+                  Næste spørgsmål
+                </Button>
+              ) : quizStarted &&
+                questionNumber + 1 === questionList.length &&
+                currentlySelectedAnswer !== "" ? (
+                <Button className="col-6" size="sm" onClick={finishQuiz}>
+                  Afslut quiz
+                </Button>
+              ) : null}
             </Col>
           </Row>
-          {quizStarted && questionNumber === questionList.length ? 
-          <p>quiz slut</p>
-          : null}
+          {quizStarted && questionNumber === questionList.length ? (
+            <p>quiz slut</p>
+          ) : null}
         </div>
       </div>
     </Container>
   );
-}
+};
 
 export default App;
